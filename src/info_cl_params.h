c26f35dd1ef04b402e47318239b9bd0ede279f63
Geert Jan BEX <geertjan.bex@uhasselt.be>
2015-07-10 16:46:02 +0200

    Added info_sink

---
+++ src/info_cl_params.h
@@ -0,0 +1,18 @@
+#ifndef INFO_CL_PARAMS_HDR
+#define INFO_CL_PARAMS_HDR
+
+#include <stdio.h>
+
+typedef struct {
+	char * sink_file;
+	int verbose;
+} Params;
+
+void initCL(Params *params);
+void parseCL(Params *params, int *argc, char **argv[]);
+void parseFileCL(Params *params, char *fileName);
+void dumpCL(FILE *fp, char prefix[], Params *params);
+void finalizeCL(Params *params);
+void printHelpCL(FILE *fp);
+
+#endif
