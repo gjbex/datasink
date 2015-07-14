a17b8bbc05e26541ad0a253d702b5746d32e91bc
Geert Jan BEX <geertjan.bex@uhasselt.be>
2015-07-14 18:58:25 +0200

    vacuum_sink reports sink overruns, and produces correct result

--- src/vacuum_sink.c
+++ src/vacuum_sink.c
@@ -30,6 +30,10 @@ int main(int argc, char *argv[]) {
     }
     for (id = 0; id < meta_data.nr_sinks; id++) {
         data_size = read_data_size(ifp, id);
+        if (data_size > meta_data.sink_size) {
+            warnx("%ld byte written to sink %ld, capacity exceede by %ld byte", data_size, id, data_size - meta_data.sink_size);
+            data_size = meta_data.sink_size;
+        }
         if (data_size < 0) {
             warnx("no data was written for id %ld\n", id);
             continue;
