bb2bb23a1c3b43c10adb3a571307aae5517a55f6
Geert Jan BEX <geertjan.bex@uhasselt.be>
2015-07-14 18:35:39 +0200

    Added overrun check/error reporting

--- src/info_sink.c
+++ src/info_sink.c
@@ -1,15 +1,19 @@
 #include <err.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "sink_utils.h"
 #include "info_cl_params.h"
 
+int compute_field_width(long nr_sinks);
+
 int main(int argc, char *argv[]) {
     Params params;
     Meta_data meta_data;
     FILE *fp;
-    long id, data_size, nr_unused = 0;
+    long id, data_size, nr_unused = 0, nr_overrun = 0;
+    int field_width;
     initCL(&params);
     parseCL(&params, &argc, &argv);
     if (params.verbose) {
@@ -22,19 +26,34 @@ int main(int argc, char *argv[]) {
     printf("meta size = %ld byte\n", meta_data.meta_size);
     printf("sink size = %ld byte\n", meta_data.sink_size);
     printf("number of sinks = %ld\n", meta_data.nr_sinks);
+    field_width = compute_field_width(meta_data.nr_sinks);
     for (id = 0; id < meta_data.nr_sinks; id++) {
         data_size = read_data_size(fp, id);
         if (data_size >= 0) {
-            printf("size id %ld = %ld byte\n", id, data_size);
+            printf("size id %5ld = %ld byte", id, data_size);
+            if (data_size > meta_data.sink_size) {
+                nr_overrun++;
+                printf(" > %ld, %ld byte lost", meta_data.sink_size,
+                       data_size - meta_data.sink_size);
+            }
+            printf("\n");
         } else {
-            printf("size id %ld unused\n", id);
+            printf("size id %5ld unused\n", id);
             nr_unused++;
         }
     }
     if (nr_unused > 0) {
         fprintf(stderr, "### warning: %ld sinks were unused\n", nr_unused);
     }
+    if (nr_overrun > 0) {
+        fprintf(stderr, "### error: %ld sinks lost data,\n", nr_overrun);
+        fprintf(stderr, "#          snik ize too small\n");
+    }
     fclose(fp);
     finalizeCL(&params);
     return EXIT_SUCCESS;
+}
+
+int compute_field_width(long nr_sinks) {
+    return (int) ceil(log(nr_sinks)/log(10));
 }
