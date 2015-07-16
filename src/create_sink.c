cd0a5f9fbe1f2c0f6a1c202ba360b3db12fdac04
Geert Jan BEX <geertjan.bex@uhasselt.be>
2015-07-16 10:26:26 +0200

    Removed duplicate line of code

--- src/create_sink.c
+++ src/create_sink.c
@@ -44,7 +44,6 @@ Meta_data convert_params(Params *params) {
     Meta_data meta_data;
     long req_size = (NR_META_DATA_FIELDS + params->nr_sinks)*sizeof(long);
     meta_data.sink_size = compute_size(params->sink_size);
-    meta_data.meta_size = compute_size(params->meta_size);
     meta_data.nr_sinks = params->nr_sinks;
     if (meta_data.meta_size < 0) {
         meta_data.meta_size = req_size;
